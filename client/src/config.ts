7c6767c1c9d220fa142fa25e5622191ad61626e7
NIshul <161936160+Nishulpatel@users.noreply.github.com>
2025-08-02 08:24:52 +0530

    Update config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
 export const BACKRND_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:3001"
-    : "https://dropithere.onrender.com";
+    : "https://dropithere-lzq3.onrender.com";
